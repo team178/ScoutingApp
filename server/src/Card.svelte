16a66c7cf5b82bd8d67ca7f8fbe3d1a14b69aa4b
Patrick Brennan <brennanpatrick20@gmail.com>
2022-04-08 21:21:06 -0400

    Removing my test app code
    It's called a test app for a reason

--- server/src/Card.svelte
+++
@@ -1,6 +0,0 @@
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text"></p>
-    </div>
-</div>
--- server/src/commands.ts
+++
@@ -1,9 +0,0 @@
-// With the Tauri API npm package:
-import { invoke } from '@tauri-apps/api/tauri'
-
-const test = () => {
-    invoke('print_db')
-    console.log('test');
-};
-
-export { test };
