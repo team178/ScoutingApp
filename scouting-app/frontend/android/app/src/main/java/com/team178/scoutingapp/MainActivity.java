5476768712c86658d6087727771c79aeb07abe5c
Patrick Brennan <brennanpatrick20@gmail.com>
2022-04-08 21:15:50 -0400

    Refactor directories
    Removing directories we don't need, and renaming some things

--- scouting-app/frontend/android/app/src/main/java/com/team178/scoutingapp/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.team178.scoutingapp;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ scoutingapp/android/app/src/main/java/com/team178/scoutingapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.team178.scoutingapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
