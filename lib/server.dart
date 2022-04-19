9be9d1323696bd9db1d2a7da418fcb9be78cbdd0
Patrick Brennan <brennanpatrick20@gmail.com>
2022-04-18 20:00:39 -0400

    Creating a simple http server to start with

--- lib/server.dart
+++ lib/server.dart
@@ -1,1 +1,14 @@
-void main() {}
+import 'package:shelf_plus/shelf_plus.dart';
+
+void main() => shelfRun(init);
+
+Handler init() {
+  var app = Router().plus;
+
+  app.post('/sync', (Request request) async {
+    var data = await request.body.asJson;
+    return data;
+  });
+
+  return app;
+}
